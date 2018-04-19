f2e0b1c3cb7888e84cded55222463a6b9a6b10b4
chgad <chgad.games@gmail.com>
2018-04-19 09:13:58 +0200

    startet golden ratio exercise

---
+++ golden_ration.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+# Function to fill a Lis with the delta fo F(n)/F(n-1) - golden ratio
+
+
+def list_fill(N):
+    golden = (1.0 + np.sqrt(5.0))*0.5
+    plot_lisst = []
+    k,v = 1,1
+    l = 0
+
+    while l<N:
+        plot_list.append()
+
+
