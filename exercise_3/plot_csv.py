c6a737276868490b9e8730ac8d9ec30d8255f000
christian <chgad.games@gmail.com>
2018-05-06 19:41:42 +0200

    proved Keplers second Law

--- exercise_3/plot_csv.py
+++ exercise_3/plot_csv.py
@@ -20,6 +20,24 @@ with open(file, 'r') as f:
         y.append(float(row[row2]))
     f.close()
 
+
+# Prove the second Kepler Law
+# if L IS constant the Integral of L dt is the same for any 2 time diferences dt_1 = dt_2
+# a realy lazymethodproduce the radial Vektor, which for elipsis changes periodically with time
+# and integrate it over the discreat time steps tau
+
+def produce_r(l):
+    return np.power(np.square(l).sum(axis=1), 1 / 2)
+
+
+r = produce_r(list(zip(x,y)))
+
+L1 = np.trapz(r[:1001], dx=0.01)
+L2 = np.trapz(r[5000:6001], dx=0.01)
+
+print("L1", L1)
+print("L2", L2)
+
 circle_x = np.cos(np.arange(0.0, 2*np.pi, 0.01))
 circle_y = np.sin(np.arange(0.0, 2*np.pi, 0.01))
 print(max(y))
@@ -34,3 +52,4 @@ plt.plot(circle_x, circle_y, label="Circle")
 # plt.plot(tau, np.array(r)-cosine(np.array(tau)), label="Absolute difference computed phi and cosine")
 plt.legend(loc="best")
 plt.show()
+
